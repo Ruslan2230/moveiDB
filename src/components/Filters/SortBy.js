672ee833f42445244e8bf50ef8d83f9ee3a4dd9d
pandakung-fu <gritsak5@gmail.com>
2020-04-20 10:02:41 +0300

    check

--- src/components/Filters/SortBy.js
+++ src/components/Filters/SortBy.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import PropTypes from "prop-types";
 
 export default class SortBy extends React.Component {
@@ -10,7 +11,7 @@ export default class SortBy extends React.Component {
   static defaultProps = {
     options: [
       {
-        label: "Популярные по убыванию",
+        label: "Популярные пощ убыванию",
         value: "popularity.desc"
       },
       {
